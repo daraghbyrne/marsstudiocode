fe57e293955ddf4140c74a63d0950d9938fe64cc
Mark Vella <Mark Vella>
2016-04-10 19:38:50 -0400

    Made two post requests CSRF exempt

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from .models import * 
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -26,14 +27,16 @@ def sensors(request):
         'sensors': Sensor.objects.all(),
     }
     return render(request, 'sensors.html', context)
-    
+
+@csrf_exempt   
 def new_data(request):
     if request.method == "POST":
         #sensor_value, timestamp, sensor_id
         d = DataPoint(sensor_id = request.POST['sensor_id'], value = request.POST['sensor_value'], gathered_at = request.POST['timestamp'])
         d.save()
         return HttpResponse()
-        
+
+@csrf_exempt        
 def new_image(request):
     if request.method == "POST":
         #image_url, timestamp, sensor_id
