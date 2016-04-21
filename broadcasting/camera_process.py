2e14a46ced9b1ab16acbfa36cefdf47e7f307a9a
Marc-Daniel Julien <marcdjulien@gmail.com>
2016-04-20 21:46:09 -0400

    Updated camera_process.py top use a single camera

--- broadcasting/camera_process.py
+++ broadcasting/camera_process.py
@@ -3,13 +3,13 @@ This code is in charge of taking pictures from the 4 cameras
 and posting them to the webserver.
 """
 import sys
-from SimpleCV import Camera
-from util import Config, logger
 import time
 import boto3
+import logging
+from SimpleCV import Camera
 from datetime import datetime
 
-PERIOD = 60.0
+PERIOD = 300.0
 IMG_POST_FMT = "image_url={}&timestamp={}&sensor_id=2"
 IMG_URL_FMT = "http://{}.s3.amazonaws.com/{}"
 
@@ -18,78 +18,62 @@ class CameraCapture(object):
     def __init__(self):
         self.image_index = 0
         
-        self.n_cameras = int(Config.get("N_CAMERAS"))
-
         # Initialize the cameras
-        self.cameras = []
-        for i in xrange(self.n_cameras):
-            try:
-                self.cameras.append(Camera(camera_index = i))
-            except:
-                logger.warning("Error opening camera #"+str(i))
+        self.cameras = Camera(camera_index = i)
         
-        self.n_cameras = len(self.cameras)
-        if self.n_cameras == 0:
-            logger.warning("Unable to find any cameras")
-            exit()
-
         # Get S3 instance for 
         self.s3 = boto3.resource('s3')
-        self.bucket_name = Config.get("BUCKET_NAME")
+        self.bucket_name = "edu.cmu.marsstudio"
+
+        # Setup logger
+        logger_format_string = '[%(processName)s:%(funcName)s][%(asctime)s][%(levelname)s]%(message)s'
+        log_format = logging.Formatter(logger_format_string)
+        self.logger = logging.getLogger('')
+        fh = logging.handlers.RotatingFileHandler("mars_log_file", maxBytes=(1048576*5), backupCount=7)
+        fh.setFormatter(log_format)
+        self.logger.addHandler(fh)
 
     def start(self):
+        self.logger.debug("Starting Cameras Process")
         while True:
             try:
                 # 1. Capture images from all cameras
                 #logger.debug("Capturing Images")
-                images = self.get_images()
+                image = self.getImage()
                 # 2. Send them to the remote server
                 #logger.debug("Submitting Images")
-                self.post_images(images)
-            except:
-                logger.warning("Unable to retrieve and send images")
+                self.post_image(image)
+            except Exception, e:
+                self.logger.warning("Unable to retrieve and send images: "+e.message)
     
             # Wait
             time.sleep(PERIOD)
 
-    def get_images(self):
-        images = []
-        for cam in self.cameras:
-            # Get Image from camera
-            img = cam.getImage()
-            images.append(img)
-        return images
+    def post_image(self, img):
+        # Upload image to S3
+        img_name = "{}.jpg".format(int(time.time()))
+        img.save("/tmp/temp_mars_img.jpg")
+        with open("/tmp/temp_mars_img.jpg", 'rb') as f:
+            self.s3.Object(self.bucket_name, img_name).put(Body=f)
 
-    def post_images(self, images):
-        for i in xrange(self.n_cameras):
-            # Upload image to S3
-            img = images[i]
-            img_name = Config.get("IMAGES_DIR")+"{}-{}.jpg".format(i, int(time.time()))
-            img.save("/tmp/temp_mars_img.jpg")
-            with open("/tmp/temp_mars_img.jpg", 'rb') as f:
-                self.s3.Object(self.bucket_name, img_name).put(Body=f)
-
-            # Send img url to webserver
-            timestamp = datetime.now().isoformat()
-            img_url = IMG_URL_FMT.format(self.bucket_name, img_name)
-            post_data = IMG_POST_FMT.format(img_url, timestamp)
-            http = httplib.HTTPConnection(Config.get("WEBSERVER_HOST_NAME"))
-            http.request("POST", Config.get("WEBSERVER_POST_IMG_URI"), post_data)
-            response = http.getresponse()
-            if response.status != 200:
-                logger.warning("Error posting images to webserver. " + response.reason)
-            else:
-                logger.debug("Image Successful: "+img_url)
-
+        # Get the time stamp
+        timestamp = datetime.now().isoformat()
+        # Create the aws image url
+        img_url = IMG_URL_FMT.format(self.bucket_name, img_name)
+        # Create the post data
+        post_data = IMG_POST_FMT.format(img_url, timestamp)
+        # Connect to the server
+        http = httplib.HTTPConnection("http://marsserver.herokuapp.com")
+        # Submit and check the request
+        http.request("POST", "new_image/", post_data)
+        response = http.getresponse()
+        if response.status != 200:
+            self.logger.warning("Error posting images to webserver. " + response.reason)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print "usage: ./camera_process.py <sensor_config> <network config file>"
-
-    # Initialize configuration
-    Config.init(sys.argv[1], sys.argv[2])
+        print "usage: ./camera_process.py"
 
     # Start
-    logger.debug("Starting Cameras Process")
     cc = CameraCapture()
     cc.start()
