694d69b520d79e8f92cee43c9629d1c78201563f
Mark Vella <Mark Vella>
2016-05-01 18:20:02 -0400

    made output json

--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,7 @@ from .images2gif import Image as ImagePackage
 from .images2gif import writeGif
 import os
 import urllib.request
+import json
 
 def get_csv(request):
     # Create the HttpResponse object with the appropriate CSV header.
@@ -27,8 +28,8 @@ def home(request):
         'images': Image.objects.order_by('-gathered_at')[:20],
         'photons': Photon.objects.order_by('name'),
         'sensors': Sensor.objects.order_by('photon_id', 'sensor_type'),
-        'data_pts': [d[0] for d in DataPoint.objects.order_by('-gathered_at').values_list('value')[:30]],
-        'data_dates': [d[0] for d in DataPoint.objects.order_by('-gathered_at').values_list('gathered_at')[:30]],
+        'data_pts': json.dumps([str(d[0]) for d in DataPoint.objects.order_by('-gathered_at').values_list('value')[:30]]),
+        'data_dates': json.dumps([d[0] for d in DataPoint.objects.order_by('-gathered_at').values_list('gathered_at')[:30]]),
     }
     return render(request, 'home.html', context)
     
